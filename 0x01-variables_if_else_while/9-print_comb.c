51548b08d2e1e2a5fabc257a799bf2e243a97bf3
Cleve-keem <belloharyourmidey@gmail.com>
2022-12-11 06:05:03 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *  * main - converts n to char, prints conversion followed by comma and space
+ *   * and increments
+ *    * Return: 0 through 9, separated by commas
+ */
+int main(void)
+{
+	int n = 0;
+	int c;
+
+	while (n <= 9)
+	{
+		c = n + '0';
+		putchar(c);
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+			n++;
+		}
+		else
+		{
+			n++;
+		}
+																}
+	putchar('\n');
+
+	return (0);
+}
