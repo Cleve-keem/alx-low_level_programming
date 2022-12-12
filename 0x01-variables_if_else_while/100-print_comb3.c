6c864b51ec33586b8bc26d5988a64df5d54c8b0a
Cleve-keem <belloharyourmidey@gmail.com>
2022-12-12 10:31:58 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,14 @@ int main(void)
 
 	while (n <= 9)
 	{
+		m = n + 1;
 		while (m <= 9)
 		{
 			c = n + '0';
 			putchar(c);
 			d = m + '0';
 			putchar(d);
-			if (n == 9 && m == 9)
+			if (n == 8 && m == 9)
 			{
 				putchar('\n');
 			}
@@ -30,7 +31,6 @@ int main(void)
 			}
 			m++;
 		}
-		m = 0;
 		n++;
 	}
 	return (0);
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *  * main - loops through variables n, m, o, printing 0-9
+ *   * Return: all possible combinations of three digits
+ */
+int main(void)
+{
+	int n = 0;
+	int m = 0;
+	int o = 0;
+
+	while (n <= 9)
+	{
+		m = n + 1;
+		while (m <= 9)
+		{
+			o = m + 1;
+			while (o <= 9)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				putchar(o + '0');
+				if (n == 7 && m == 8 && o == 9)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(',');
+
