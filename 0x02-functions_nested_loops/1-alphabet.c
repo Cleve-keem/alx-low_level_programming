86493d82aaed7e757e9288bfdc2c3a3fd3d55eb9
Cleve-keem <belloharyourmidey@gmail.com>
2022-12-14 02:11:18 -0800

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - prints alphabet using _putchar
+ *   * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = a;
+	
+	while (c <='z')
+	{
+		_putchar(c);
+		c++;
+	}
+	return (0);
+}
