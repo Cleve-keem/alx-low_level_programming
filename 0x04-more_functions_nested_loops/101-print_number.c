e57424c4df0f5e8d95362646c13ac25751fb02ab
Cleve-keem <belloharyourmidey@gmail.com>
2022-12-15 09:18:44 -0800

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,13 @@
  */
 void print_number(int n)
 {
-	int count, digit, i;
-	int div = 10;
-
-        while (n != 0)
-	{
-		n = n / 10;
-		count++;
-	}
-
+	unsigned int num = n;
 	if (n < 0)
-		_putchar('-');
-
-        for (i = count; i >= 0; i--)
 	{
-		digit = n / div;
-		_putchar(digit + '0');
+		_putchar('-');
+		num = -num;
 	}
+	if ((num / 10) > 0)
+		print_number(num /10);
+	_puthchar((num % 10) + '0');
 }
