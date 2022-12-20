b6cabd182d971b0c99a021a0942a2d6c9f273a2c
Cleve-keem <belloharyourmidey@gmail.com>
2022-12-20 09:59:03 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string followed by a new line
+ * @str: string to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, k;
+	int j = _strlen(str);
+
+	if (j % 2 == 0)
+		k = j / 2;
+	else
+		k = (j / 2) + 1;
+
+	for (i = k; i < j; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
