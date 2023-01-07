f3e314f40de7c55f4b8bc5e1a6e5a3e244f5a628
Cleve-keem <belloharyourmidey@gmail.com>
2023-01-06 23:29:24 -0800

    my favouritr task

---
+++ 0x09-static_libraries/1-strcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: string to be appended
+ * @n: maximum bytes used from src
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j = _strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[j + i] = src[i];
+	dest[j + i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies string pointed to by src to the buffer pointed to dest
+ * @dest: string destination
+ * @src: string source
+ * @n: number of bytes to be used from src
+ * Return: the pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
